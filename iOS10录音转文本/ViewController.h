c79ce43917a6a60dc40e3344867d85f6b44747cf
hekr <1195689411@qq.com>
2016-09-13 11:47:08 +0800

    push

---
+++ iOS10录音转文本/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS10录音转文本
+//
+//  Created by Memebox on 16/9/11.
+//  Copyright © 2016年 Hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
